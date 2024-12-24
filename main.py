9637de11c9f8c5c33a0d46c54f022ee805ca3cd6
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2024-12-24 23:42:33 +0700

    added: plugin content

--- main.py
+++ main.py
@@ -5,53 +5,72 @@ import os
 # and add the `decky-loader/plugin/imports` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky
 import asyncio
+from settings import SettingsManager
+import logging
+
+RESOLVED_DNS_CONFIG_PATH="/etc/systemd/resolved.conf.d/decky-dot-dns.conf"
+SETTINGS_DIR = os.environ["DECKY_PLUGIN_SETTINGS_DIR"]
 
 class Plugin:
-    # A normal method. It can be called from the TypeScript side using @decky/api.
-    async def add(self, left: int, right: int) -> int:
-        return left + right
+    SettingsManager = SettingsManager(SETTINGS_DIR, "settings.json")
+    
+    async def push_custom_dns_override(self, insecure_dns: str, secure_dns: str, insecure_dnsv6: str, secure_dnsv6: str, use_dot: bool = False):
+        logging.info(f"Pushing custom dns override: insecure_dns={insecure_dns}, secure_dns={secure_dns}, insecure_dnsv6={insecure_dnsv6}, secure_dnsv6={secure_dnsv6}, use_dot={use_dot}")
+        try:
+            # Ensure that the path exists
+            os.makedirs(os.path.dirname(RESOLVED_DNS_CONFIG_PATH), exist_ok=True)
 
-    async def long_running(self):
-        await asyncio.sleep(15)
-        # Passing through a bunch of random data, just as an example
-        await decky.emit("timer_event", "Hello from the backend!", True, 2)
+            dns_string = f"{insecure_dns}" + (f"#{secure_dns}" if use_dot else "")
+            dns_string = f"{dns_string} {insecure_dnsv6}" + (f"#{secure_dnsv6}" if use_dot else "")
+            file_content = f"""
+[Resolve]
+DNS={dns_string}
+DNSOverTLS={"yes" if use_dot else "no"}
+"""        
+            with open(RESOLVED_DNS_CONFIG_PATH, "w") as f:
+                f.write(file_content)
 
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
-    async def _main(self):
-        self.loop = asyncio.get_event_loop()
-        decky.logger.info("Hello World!")
+            self.SettingsManager.setSetting("insecure_dns", insecure_dns)
+            self.SettingsManager.setSetting("secure_dns", secure_dns)
+            self.SettingsManager.setSetting("insecure_dnsv6", insecure_dnsv6)
+            self.SettingsManager.setSetting("secure_dnsv6", secure_dnsv6)
+            self.SettingsManager.setSetting("use_dot", use_dot)
+            
+            logging.info(f"Pushed custom dns override: {file_content}")
+            await self.restart_resolved()
+        except Exception as e:
+            logging.error(f"Failed to push custom dns override: {e}")
 
-    # Function called first during the unload process, utilize this to handle your plugin being stopped, but not
-    # completely removed
+    async def drop_custom_dns_override(self):
+        if os.path.exists(RESOLVED_DNS_CONFIG_PATH):
+            os.remove(RESOLVED_DNS_CONFIG_PATH)
+            logging.info(f"Removed custom dns override")
+        else:
+            logging.info(f"Custom dns override not found")
+
+        await self.restart_resolved()
+
+    async def restart_resolved(self):
+        os.system("systemctl restart systemd-resolved")
+        logging.info(f"Restarted systemd-resolved")
+
+    async def get_dns_settings(self):
+        return [
+            self.SettingsManager.getSetting("insecure_dns"),
+            self.SettingsManager.getSetting("secure_dns"),
+            self.SettingsManager.getSetting("insecure_dnsv6"),
+            self.SettingsManager.getSetting("secure_dnsv6"),
+            self.SettingsManager.getSetting("use_dot")
+        ]   
+
     async def _unload(self):
-        decky.logger.info("Goodnight World!")
+        await self.drop_custom_dns_override()
+        await self.restart_resolved()
         pass
 
-    # Function called after `_unload` during uninstall, utilize this to clean up processes and other remnants of your
-    # plugin that may remain on the system
     async def _uninstall(self):
-        decky.logger.info("Goodbye World!")
+        await self._unload()
         pass
 
-    async def start_timer(self):
-        self.loop.create_task(self.long_running())
-
-    # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
-        decky.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky.decky_LOG_DIR/template.log`
-        decky.migrate_logs(os.path.join(decky.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky.decky_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky.decky_SETTINGS_DIR/`
-        decky.migrate_settings(
-            os.path.join(decky.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky.decky_RUNTIME_DIR/`
-        decky.migrate_runtime(
-            os.path.join(decky.DECKY_HOME, "template"),
-            os.path.join(decky.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        pass
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import {
   ButtonItem,
+  Dropdown,
   PanelSection,
   PanelSectionRow,
-  Navigation,
-  staticClasses
+  TextField,
+  ToggleField,
+  staticClasses,
+  SingleDropdownOption
 } from "@decky/ui";
 import {
-  addEventListener,
-  removeEventListener,
   callable,
   definePlugin,
-  toaster,
   // routerHook
 } from "@decky/api"
-import { useState } from "react";
-import { FaShip } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaNetworkWired } from "react-icons/fa";
 
 // import logo from "../assets/logo.png";
 
@@ -22,94 +22,159 @@ import { FaShip } from "react-icons/fa";
 // Note the type annotations:
 //  the first one: [first: number, second: number] is for the arguments
 //  the second one: number is for the return value
-const add = callable<[first: number, second: number], number>("add");
+const push_custom_dns_override = callable<[string, string, string, string, boolean], void>("push_custom_dns_override");
+const drop_custom_dns_override = callable<[], void>("drop_custom_dns_override");
+const restart_resolved = callable<[], void>("restart_resolved");
+const get_dns_settings = callable<[], [string, string, string, string, boolean]>("get_dns_settings");
 
-// This function calls the python function "start_timer", which takes in no arguments and returns nothing.
-// It starts a (python) timer which eventually emits the event 'timer_event'
-const startTimer = callable<[], void>("start_timer");
+class DNSTemplate {
+  name: string;
+  insecure_dns: string;
+  secure_dns: string;
+  insecure_dnsv6: string;
+  secure_dnsv6: string;
+  use_dot: boolean;
+  constructor(name: string, insecure_dns: string, secure_dns: string, insecure_dnsv6: string, secure_dnsv6: string, use_dot: boolean) {
+    this.name = name;
+    this.insecure_dns = insecure_dns;
+    this.secure_dns = secure_dns;
+    this.insecure_dnsv6 = insecure_dnsv6;
+    this.secure_dnsv6 = secure_dnsv6;
+    this.use_dot = use_dot;
+  }
+}
+
+const DNSTemplates = {
+  "None": new DNSTemplate("None", "", "", "", "", false),
+  "CloudFlare": new DNSTemplate("CloudFlare", "1.1.1.1", "cloudflare-dns.com", "2606:4700:4700::1111", "cloudflare-dns.com", false),
+  "Google": new DNSTemplate("Google", "8.8.8.8", "dns.google", "2001:4860:4860::8888", "dns.google", true),
+  "dns0.eu": new DNSTemplate("dns0.eu", "193.110.81.0", "dns0.eu", "2a0f:fc80::", "dns0.eu", true),
+  "Quad9": new DNSTemplate("Quad9", "9.9.9.10", "dns10.quad9.net", "2620:fe::10", "dns10.quad9.net", true)
+}
 
 function Content() {
-  const [result, setResult] = useState<number | undefined>();
+  const [insecure_dns, setInsecureDNS] = useState<string>("");
+  const [secure_dns, setSecureDNS] = useState<string>("");
+  const [insecure_dnsv6, setInsecureDNSv6] = useState<string>("");
+  const [secure_dnsv6, setSecureDNSv6] = useState<string>("");
+  const [use_dot, setUseDot] = useState<boolean>(false);
+  const [selectedPreset, setSelectedPreset] = useState<string>("None");
 
-  const onClick = async () => {
-    const result = await add(Math.random(), Math.random());
-    setResult(result);
+  const onSubmit = async () => {
+    await push_custom_dns_override(insecure_dns, secure_dns, insecure_dnsv6, secure_dnsv6, use_dot);
+    await restart_resolved();
+  };
+
+  const onDrop = async () => {
+    await drop_custom_dns_override();
+    await restart_resolved();
+  };
+
+  useEffect(() => {
+    get_dns_settings().then(([insecure_dns, secure_dns, insecure_dnsv6, secure_dnsv6, use_dot]) => {
+      setInsecureDNS(insecure_dns);
+      setSecureDNS(secure_dns);
+      setInsecureDNSv6(insecure_dnsv6);
+      setSecureDNSv6(secure_dnsv6);
+      setUseDot(use_dot);
+    });
+  }, []);
+
+  const presetOptions: SingleDropdownOption[] = Object.keys(DNSTemplates).map(key => {
+    return {
+      label: <span>{key}</span>,
+      data: DNSTemplates[key as keyof typeof DNSTemplates]
+    }
+  })
+
+  const onSelectPreset = async (preset: DNSTemplate) => {
+    setInsecureDNS(preset.insecure_dns);
+    setSecureDNS(preset.secure_dns);
+    setInsecureDNSv6(preset.insecure_dnsv6);
+    setSecureDNSv6(preset.secure_dnsv6);
+    setUseDot(preset.use_dot);
   };
 
   return (
-    <PanelSection title="Panel Section">
+    <PanelSection>
+      <div className={staticClasses.PanelSectionTitle}>
+        {"DNS Presets"}
+      </div>
       <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={onClick}
-        >
-          {result ?? "Add two numbers via Python"}
-        </ButtonItem>
-      </PanelSectionRow>
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => startTimer()}
-        >
-          {"Start Python timer"}
-        </ButtonItem>
-      </PanelSectionRow>
-
-      {/* <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow> */}
-
-      {/*<PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.Navigate("/decky-plugin-test");
-            Navigation.CloseSideMenus();
+        <Dropdown
+          strDefaultLabel="Select a preset"
+          selectedOption={presetOptions.find((value: SingleDropdownOption, _index, _array) => value.data.name === selectedPreset)}
+          rgOptions={presetOptions}
+          onChange={(elem: SingleDropdownOption) => {
+            setSelectedPreset(elem.data.name);
+            onSelectPreset(elem.data as DNSTemplate);
           }}
+        />
+      </PanelSectionRow>
+
+      <div className={staticClasses.PanelSectionTitle}>
+        {"Custom DNS"}
+      </div>
+      <PanelSectionRow>
+        <ToggleField
+          label="Use DoT"
+          checked={use_dot}
+          onChange={setUseDot}
+        />
+        <TextField
+          label="Insecure DNS"
+          value={insecure_dns}
+          onChange={e => setInsecureDNS(e.target.value)}
+        />
+
+        <TextField
+          label="Secure DNS"
+          value={secure_dns}
+          onChange={e => setSecureDNS(e.target.value)}
+          disabled={!use_dot}
+        />
+
+        <TextField
+          label="Insecure DNSv6"
+          value={insecure_dnsv6}
+          onChange={e => setInsecureDNSv6(e.target.value)}
+        />
+
+        <TextField
+          label="Secure DNSv6"
+          value={secure_dnsv6}
+          onChange={e => setSecureDNSv6(e.target.value)}
+          disabled={!use_dot}
+        />
+      </PanelSectionRow>
+
+      <PanelSectionRow>
+        <ButtonItem
+          layout="below"
+          onClick={onSubmit}
         >
-          Router
+          Submit
         </ButtonItem>
-      </PanelSectionRow>*/}
+
+        <ButtonItem
+          layout="below"
+          onClick={onDrop}
+        >
+          Restore Defaults
+        </ButtonItem>
+      </PanelSectionRow>
     </PanelSection>
   );
 };
 
 export default definePlugin(() => {
-  console.log("Template plugin initializing, this is called once on frontend startup")
-
-  // serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-  //   exact: true,
-  // });
-
-  // Add an event listener to the "timer_event" event from the backend
-  const listener = addEventListener<[
-    test1: string,
-    test2: boolean,
-    test3: number
-  ]>("timer_event", (test1, test2, test3) => {
-    console.log("Template got timer_event with:", test1, test2, test3)
-    toaster.toast({
-      title: "template got timer_event",
-      body: `${test1}, ${test2}, ${test3}`
-    });
-  });
-
   return {
-    // The name shown in various decky menus
-    name: "Test Plugin",
-    // The element displayed at the top of your plugin's menu
-    titleView: <div className={staticClasses.Title}>Decky Example Plugin</div>,
-    // The content of your plugin's menu
+    name: "Decky DNS Settings",
+    titleView: <div className={staticClasses.Title}>Decky DNS Settings</div>,
     content: <Content />,
-    // The icon displayed in the plugin list
-    icon: <FaShip />,
-    // The function triggered when your plugin unloads
+    icon: <FaNetworkWired />,
     onDismount() {
       console.log("Unloading")
-      removeEventListener("timer_event", listener);
-      // serverApi.routerHook.removeRoute("/decky-plugin-test");
     },
   };
 });
